e56f277042f0d0cf4cd1d1b601e84f306d3c0561
Ian Hirschfeld <ianh7976@gmail.com>
2015-03-07 15:24:00 -0800

    Updates admin section with starter styles.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,17 +1,6 @@
 class AdminController < ProtectedController
 
-  before_action :require_login
-  before_action :require_admin
-
   def show
-  end
-
-  private
-
-  def require_admin
-    if signed_out? || !current_user.admin?
-      redirect_to root_path
-    end
   end
 
 end
