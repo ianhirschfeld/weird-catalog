5e7351bb2c1b03180a5f07f109b8fb8cc760d368
Ian Hirschfeld <ianh7976@gmail.com>
2015-05-25 18:27:04 -0700

    Only sends to buffer if the queue isn't full.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,23 +21,27 @@ module ApplicationHelper
 
   def send_item_to_buffer(item)
     buffer = Buffer::Client.new ENV['BUFFER_ACCESS_TOKEN']
-    buffer_profiles = buffer.profiles
+    profiles = buffer.profiles
+    pending = buffer.updates_by_profile_id profiles.first[:id], status: 'pending'
     data = {body: {shorten: true}}
 
-    buffer_profiles.each do |profile|
-      case profile[:service]
-      when 'facebook'
-        data[:body][:profile_ids] = [profile[:id]]
-        data[:body][:text] = facebook_share_text(item)
-        buffer.create_update data
-      when 'twitter'
-        data[:body][:profile_ids] = [profile[:id]]
-        data[:body][:text] = twitter_share_text(item)
-        buffer.create_update data
+    # 10 posts is the max allowed in queue for free accounts.
+    if pending.total < 10
+      profiles.each do |profile|
+        case profile[:service]
+        when 'facebook'
+          data[:body][:profile_ids] = [profile[:id]]
+          data[:body][:text] = facebook_share_text(item)
+          buffer.create_update data
+        when 'twitter'
+          data[:body][:profile_ids] = [profile[:id]]
+          data[:body][:text] = twitter_share_text(item)
+          buffer.create_update data
+        end
       end
-    end
 
-    item.update_attributes send_to_buffer: false
+      item.update_attributes send_to_buffer: false
+    end
   end
 
 end
